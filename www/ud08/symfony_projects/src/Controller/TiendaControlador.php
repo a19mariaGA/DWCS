53d51836aacf9ecac0d3f6cb2b7b1fa3ef1a5fae
Maria <A19MariaGA@iessanclemente.net>
2024-05-14 21:13:35 +0200

    punto 2

--- www/ud08/symfony_projects/src/Controller/TiendaControlador.php
+++ www/ud08/symfony_projects/src/Controller/TiendaControlador.php
@@ -6,10 +6,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 
-
-class FruteriaControlador extends AbstractController{
+class TiendaControlador extends AbstractController{
 
   
     #[Route('/')]
@@ -75,6 +75,23 @@ class FruteriaControlador extends AbstractController{
         ]);
     }
 
+
+
+   
+
+    #[Route('/listaAnimales/{slug}')]
+    public function mascotas(HttpClientInterface $httpClient, string $slug = null): Response
+    {
+
+        $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/sabelassm/json-example/master/animals-1.json');
+        $animales = $response->toArray();
+
+
+        return $this->render("animales/listaAnimales.html.twig", [
+           'animales' => $animales,
+           
+        ]);
+    }
 
 
 }
